d82d98113c7e1eeefff288aea207be889e609e35
justakline <50549915+justakline@users.noreply.github.com>
2023-05-05 10:36:24 -0400

    Created the rough cut of the parser, needs modification for correct output

--- parser.fsx
+++ parser.fsx
@@ -1,4 +1,3 @@
-
 (* Parser Sample *)
 
 (* NOTE: This example uses generic OCaml compatable syntax where the syntax where possible, which is
@@ -11,19 +10,19 @@
 
 // A sample grammar
 //
-// sentence	: np vp np sentence_tail 
-// sentence_tail	: conj sentence | eos 
-// np       	: art adj_list noun pp  	 
-// adj_list 	: adj adj_tail | ε  
-// adj_tail 	: comma adj_list | ε     // comma as in “,” 
-// pp       	: prep np | ε  
-// vp       	: adv verb | verb 
+// sentence	: np vp np sentence_tail
+// sentence_tail	: conj sentence | eos
+// np       	: art adj_list noun pp
+// adj_list 	: adj adj_tail | ε
+// adj_tail 	: comma adj_list | ε     // comma as in “,”
+// pp       	: prep np | ε
+// vp       	: adv verb | verb
 
 // Tokens
 type Token =
     | Add_Op of string
     | Mult_Op of string
-    | Rel of string
+    | Rel_Op of string
     | Left_Par of string
     | Right_Par of string
     | Assign of string
@@ -33,39 +32,39 @@ type Token =
     | Then of string // a comma is just a comma
     | Else of string
     | Fi of string
-    | For of string  // don't care what the conjunction is
-    | To of string  // don't care what the conjunction is
-    | Do of string  // don't care what the conjunction is
-    | Done of string  // don't care what the conjunction is
-    | Step of string  // don't care what the conjunction is
+    | For of string // don't care what the conjunction is
+    | To of string // don't care what the conjunction is
+    | Do of string // don't care what the conjunction is
+    | Done of string // don't care what the conjunction is
+    | Step of string // don't care what the conjunction is
+    | Equals of string
     | Id of string
 
-    with static member tokenFromLexeme str = // Function to get a token from a lexeme (String)
-            match str with
-            | "+" | "-" -> Add_Op str
-            | "*" | "/" -> Mult_Op str
-            | "<" | ">" | "==" -> Rel str
-            | "("  -> Left_Par str
-            | ")"  -> Right_Par str
-            | ":="  -> Assign str
-            | "read"  -> Read str
-            | "write"  -> Write str
-            | "if"  -> If str
-            | "then"  -> Then str
-            | "else"  -> Else str
-            | "fi"  -> Fi str
-            | "for"  -> For str
-            | "to"  -> To str
-            | "do"  -> Do str
-            | "done"  -> Done str
-            | "step"  -> Step str
-            | x -> Id x
-
-
-
-
-
-
+    static member tokenFromLexeme str = // Function to get a token from a lexeme (String)
+        match str with
+        | "+"
+        | "-" -> Add_Op str
+        | "*"
+        | "/" -> Mult_Op str
+        | "<"
+        | ">"
+        | "==" -> Rel_Op str
+        | "(" -> Left_Par str
+        | ")" -> Right_Par str
+        | ":=" -> Assign str
+        | "read" -> Read str
+        | "write" -> Write str
+        | "if" -> If str
+        | "then" -> Then str
+        | "else" -> Else str
+        | "fi" -> Fi str
+        | "for" -> For str
+        | "to" -> To str
+        | "do" -> Do str
+        | "done" -> Done str
+        | "step" -> Step str
+        | "=" -> Equals str
+        | x -> Id x
 
 
 // NOTES:
@@ -76,49 +75,159 @@ type Token =
 let rec parse (theList: Token list) = program theList
 
 //program ⟶ stmt_list
-and program lst = lst |> stmt_lst
+and program lst = lst |> stmt_list
 
-// stmt_list ⟶ stmt stmt_list | ε
+// stmt_list ⟶ (stmt stmt_list) | ε
 //  stmt ⟶ assignment | read_stmt | write_stmt | for_loop | if_stmt    // so need to use these tokens
-and stmt_list = function
-    
+and stmt_list =
+    function
 
+    | Id x :: xs -> Id x :: xs |> stmt |> stmt_list
+    | Read x :: xs -> Read x :: xs |> stmt |> stmt_list
+    | Write x :: xs -> Write x :: xs |> stmt |> stmt_list
+    | If x :: xs -> If x :: xs |> stmt |> stmt_list
+    | xs -> xs // Empty
 
-    xs -> xs |> stmt
-    and ->
-    | Id x :: xs -> xs stmt_list
-    | Read x :: xs -> xs stmt_list
-    | Write x :: xs -> xs stmt_list
-    | If x :: xs -> xs if_stmt_list
-    | xs -> xs
+//  stmt ⟶ assignment | read_stmt | write_stmt | for_loop | if_stmt    // so need to use these tokens
+and stmt =
+    function
+    | Id x :: xs -> Id x :: xs |> assignment
+    | Read x :: xs -> Read x :: xs |> read_stmt
+    | Write x :: xs -> Write x :: xs |> write_stmt
+    | For x :: xs -> For x :: xs |> for_loop
+    | If x :: xs -> If x :: xs |> if_stmt
+    | _ -> failwith "Unexpected end of input while processing stmt."
 
-and stmt = function
-    | Id x :: xs -> xs |> assignment 
-    | Read x :: xs -> xs |> read_stmt
-    | Write x :: xs -> xs |>write_stmt
-    | If x :: xs -> xs |> if_stmt
-    | [] -> failwith "Unexpected end of input while processing stmt Phrase."
 // assignment ⟶ id := expr
-and assignment = function
+and assignment =
+    function
     | Id x :: Assign a :: xs -> xs |> expr
-    | x :: xs -> failwithf $"Expected ID, but found: %A{x}"
+    | x :: xs -> failwithf $"Expected Id, but found: %A{x} at assignment"
     | [] -> failwith "assignment should not be empty"
 
 // expr ⟶ term term_tai
-// term ⟶ factor factor_tail 
-// factor ⟶ ( expr ) | id  // So To find out the pattern mach is correct, I have to see if it fits this
-and expr = function
-    | Left_Par x :: xs -> xs |> expr -> Right_Par x::xs   
-    
+and expr =
+    function
+    | xs -> xs |> term |> term_tail
+
+// term ⟶ factor factor_tail
+and term =
+    function
+    | xs -> xs |> factor |> factor_tail
+
+//term_tail ⟶ add_op term term_tail | ε
+and term_tail =
+    function
+    | Add_Op x :: xs -> xs |> term |> term_tail
+    | xs -> xs //empty
+
+// factor ⟶ ( expr ) | id
+//Pattern match left parenth and pipe to expr, then pipe that result to a function to see if there is a right parenth
+and factor =
+    function
+    | Left_Par x :: xs ->
+        let result = xs |> expr
+
+        result
+        |> (function
+        | Right_Par x :: xs -> xs
+        | [] -> failwith "factor should have a right parenthesis"
+        | _ -> failwith "Unexpected pattern")
+    | Id x :: xs -> xs
+    | [] -> failwith "factor should not be empty"
+    | _ -> failwith "Unexpected pattern in factor"
+
+//factor_tail ⟶ mult_op factor factor_tail | ε
+and factor_tail =
+    function
+    | Mult_Op x :: xs -> xs |> factor |> factor_tail
+    | xs -> xs //Empty
+
+//cond ⟶ expr rel_oper expr
+and cond =
+    function
+    | xs ->
+        let result = xs |> expr
+
+        result
+        |> (function
+        | Rel_Op result :: xs -> xs |> expr
+        | [] -> failwith "Unexpected pattern in cond"
+        | _ -> failwith "Unexpected pattern in cond")
+
+// read_stmt ⟶ read id
+and read_stmt =
+    function
+    | Read x :: Id id :: xs -> xs
+    | _ -> failwith "Unexpected pattern in read"
+
+// write_stmt ⟶ write expr
+and write_stmt =
+    function
+    | Write x :: xs -> xs |> expr
+    | _ -> failwith "Unexpected pattern in write"
+
+// if_stmt ⟶ if cond then stmt_list else_stmt
+// Pattern match If, send to cond, then pattern match With then, and send to stmt_list and else_stmt
+and if_stmt =
+    function
+    | If x :: xs ->
+        let r1 = xs |> cond
+
+        r1
+        |> (function
+        | Then y :: ys -> ys |> stmt_list |> else_stmt
+        | _ -> failwith "Unexpected Pattern at the Then")
+
+    | _ -> failwith "Unexpected Pattern at the if"
+
+//else_stmt ⟶ else stmt_list fi | fi
+and else_stmt =
+    function
+    | Fi x :: xs -> xs
+    | Else x :: xs ->
+        let result = xs |> stmt_list
+
+        result
+        |> (function
+        | Fi y :: ys -> ys
+        | _ -> failwith "Unexpected Pattern at else end of stmt")
+
+    | _ -> failwith "Unexpected Pattern at else"
+
+// for_loop ⟶ for id = id to id step_stmt do stmt_list done
+//Pattern match from for to step_stmt, pipe into a function to pattern match do, then pipe into stmt_list and pattern match done
+and for_loop =
+    function
+    | For x :: Id id1 :: Equals equals :: Id id2 :: To t :: xs ->
+        let r1 = xs |> step_stmt
+
+        r1
+        |> (function
+        | Do d :: ds ->
+            let r2 = ds |> stmt_list
+
+            r2
+            |> (function
+            | Done y :: ys -> ys
+            | _ -> failwith "Unexpected Pattern at Done of for_loop")
+        | _ -> failwith "Unexpected Pattern at Do of for_loop")
+    | _ -> failwith "Unexpected Pattern at For of for_loop"
+
+and step_stmt =
+    function
+    | Step x :: Id id :: xs -> xs
+    | xs -> xs
+
+
+
+
 
 (* Begin Parsing Process *)
-let startParsing (str:string) =
+let startParsing (str: string) =
     // Split the String (which creates an Array) -> convert the Array to a List -> MAP the list of strings into a list of Tokens.
     // (Note, though arrays are a lot like lists, lists are a bit easier to use for the pattern matching.)
-    let tokenList =
-        str.Split ' ' |>
-        Array.toList |>
-        List.map Token.tokenFromLexeme
+    let tokenList = str.Split ' ' |> Array.toList |> List.map Token.tokenFromLexeme
 
     // Display our list of tokens... just for fun.
     printfn $"The initial String: %s{str}"
@@ -126,10 +235,10 @@ let startParsing (str:string) =
 
     // Work the magic...
     try
-        let parsedList = program tokenList
-        in printfn $"The Final List:\n\t%A{parsedList}"
-    with
-        Failure msg -> printfn $"Error: %s{msg}";  System.Console.ReadLine () |> ignore
+        let parsedList = program tokenList in printfn $"The Final List:\n\t%A{parsedList}"
+    with Failure msg ->
+        printfn $"Error: %s{msg}"
+        System.Console.ReadLine() |> ignore
 
 
 
@@ -143,11 +252,13 @@ let promptAndGo () =
     // let userInput = "the noun chases the adj cat and the adj , adj , fast dog adv chases the cat prep a adj noun ."
 
     let userInput =
-        printf "Enter String: ";
+        printf "Enter String: "
         // A case where it's easier to use the .NET ReadLine as opposed to the more restrictive OCaml native variant.
-        System.Console.ReadLine ()
+        System.Console.ReadLine()
 
-    in startParsing userInput
+    in
+
+    startParsing userInput
 
 
 // RUN INTERACTIVELY AS A SCRIPT
